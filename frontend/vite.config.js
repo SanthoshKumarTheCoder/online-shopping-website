7b90c62725fcabf7c7293bd1a93079b5748389fc
SANTHOSH KUMAR.G <sksanthoshkumarsk007@gmail.com>
2025-07-10 14:56:03 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
    base: './',
-  server: {
-    proxy: {
-      '/api': 'https://ba-ua9j.onrender.com', // 👈 Proxy API calls to backend
-    },
-  },
+ 
 })
