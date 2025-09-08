ba734886626df89de6c5852a863afe5f06613906
SANTHOSH KUMAR.G <sksanthoshkumarsk007@gmail.com>
2025-09-08 22:39:42 +0530

    Update DressDeatailes.jsx

--- frontend/src/pages/dress/DressDeatailes.jsx
+++ frontend/src/pages/dress/DressDeatailes.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import {  useParams } from 'react-router-dom';
 import { StoreContext } from '../../components/context/Storecontext';
 import { IoIosAdd, IoIosRemove } from "react-icons/io";
+import DressLoadersList from './DressLoader';
 
 
 function DressDeatailes() {
@@ -21,7 +22,7 @@ function DressDeatailes() {
             .catch(err => console.error("Failed to fetch item:", err));
         }, [id, url]);
       
-        if (!item) return <p>Loading item details...</p>;
+        if (!item) return   <DressLoadersList/>;
       
         return (
           <div className="item-detail-page"  onMouseEnter={() => setHovered(true)}>
